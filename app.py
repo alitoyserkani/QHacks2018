b719fa6690cc742b3612602916d3e7d651f8bdc7
Patrick Liu <pcjl@users.noreply.github.com>
2018-02-03 12:25:43 -0500

    Images

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import os
 
 # from get_rep import get_rep
 from classify import classify
@@ -9,8 +10,8 @@ app = flask.Flask(__name__)
 @app.route('/get_user', methods=['POST'])
 def get_user():
     image = flask.request.files['image']
-    import pdb; pdb.set_trace
-    user, confidence = classify(image)
+    image.save(os.path.join('/root/haven/', 'image.jpg'))
+    user, confidence = classify(os.path.join('/root/haven/', 'image.jpg'))
 
     if user is None:
         return flask.Response(
