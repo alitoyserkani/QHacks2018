da661b96b103844c5a46d905f4313889bc6d8e8f
Patrick Liu <pcjl@users.noreply.github.com>
2018-02-03 07:29:30 -0500

    Clear cache in script

--- generate_classifier.sh
+++ generate_classifier.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm ./aligned-images/cache.t7
 ../openface/util/align-dlib.py ./training-images/ align outerEyesAndNose ./aligned-images/ --size 96
 ../openface/batch-represent/main.lua -outDir ./generated-embeddings/ -data ./aligned-images/
 ../openface/demos/classifier.py train ./generated-embeddings/
