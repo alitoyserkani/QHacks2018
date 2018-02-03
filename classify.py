e86ef36ce00172a11f8d74c0a4cb6c47e0f50650
Vivien Ding <vivienzding@gmail.com>
2018-02-03 07:55:35 -0500

    Add classifying function

---
+++ classify.py
@@ -0,0 +1,27 @@
+from sklearn.mixture import GMM
+import numpy as np
+import openface
+import pickle
+
+
+def infer(img, args):
+    with open('generated-embeddings/classifier.pkl', 'r') as f:
+        (le, clf) = pickle.load(f)
+
+    rep, bounding_box = get_rep(img)
+
+    try:
+        rep = rep.reshape(1, -1)
+    except:
+        print("No face detected")
+        return None
+
+    predictions = clf.predict_proba(rep).ravel()
+    maxI = np.argmax(predictions)
+    person = le.inverse_transform(maxI)
+    confidence = predictions[maxI]
+
+    if isinstance(clf,GMM):
+        dist = np.linalg.norm(rep - clf.means_[maxI])
+
+    return (person, confidence)
