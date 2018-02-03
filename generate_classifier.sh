ac841098961dad6ed2135749e203fc1e9a1a42b3
Patrick Liu <pcjl@users.noreply.github.com>
2018-02-03 07:19:53 -0500

    Add script to quickly generate classifier

---
+++ generate_classifier.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+../openface/util/align-dlib.py ./training-images/ align outerEyesAndNose ./aligned-images/ --size 96
+../openface/batch-represent/main.lua -outDir ./generated-embeddings/ -data ./aligned-images/
+../openface/demos/classifier.py train ./generated-embeddings/
