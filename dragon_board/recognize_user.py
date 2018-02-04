87d558454ff3ce6b27360965a8f3dde79f915bbb
Patrick Liu <pcjl@users.noreply.github.com>
2018-02-04 07:06:46 -0500

    Add motor functionality

--- dragon_board/recognize_user.py
+++ dragon_board/recognize_user.py
@@ -1,12 +1,11 @@
+from gpio_96boards import GPIO
+# from ggts import gTTS
 import cv2
 import json
-import sys
-import requests
 import os
+import requests
+import sys
 import time
-
-from gpio_96boards import GPIO
-#from ggts import gTTS
 
 DEVICE_NUMBER = 0
 IMAGE_FILE = "output.jpg"
@@ -14,16 +13,28 @@ API = "http://ec2-18-219-99-191.us-east-2.compute.amazonaws.com:5000/verify"
 no_face = True
 
 GPIO_A = GPIO.gpio_id('GPIO_A')
-pins = (
-     (GPIO_A, 'out'),
+GPIO_C = GPIO.gpio_id('GPIO_C')
+GPIO_E = GPIO.gpio_id('GPIO_E')
+GPIO_I = GPIO.gpio_id('GPIO_I')
+GPIO_K = GPIO.gpio_id('GPIO_K')
+light_pins = (
+    (GPIO_A, 'out'),
+)
+motor_pins = (
+    (GPIO_C, 'out'),
+    (GPIO_E, 'out'),
+    (GPIO_I, 'out'),
+    (GPIO_K, 'out'),
 )
 
 
 def turn_on(gpio):
     gpio.digital_write(GPIO_A, GPIO.HIGH)
 
+
 def turn_off(gpio):
     gpio.digital_write(GPIO_A, GPIO.LOW)
+
 
 def main():
     while no_face:
@@ -54,23 +65,36 @@ def main():
                 # tts = gTTS(text, lang='en')
                 # tts.save("welcome_msg.mp3")
                 # os.system("mp321 welcome_msg.mp3")
-                
-                with GPIO(pins) as gpio:
+
+                with GPIO(light_pins) as gpio:
                     turn_on(gpio)
+                with GPIO(motor_pins) as gpio:
+                    turn_on(gpio)
+                    time.sleep(3)
+                    turn_off(gpio)
+
                 # filename = sys/class/gpio/gpio12
                 # os.system('./Electronics/turnOnGPIO.sh')
 
                 k = raw_input("")
                 if k == 'q':
-                    with GPIO(pins) as gpio:
+                    with GPIO(light_pins) as gpio:
+                        turn_off(gpio)
+                    with GPIO(motor_pins) as gpio:
+                        turn_on(gpio)
+                        time.sleep(3)
                         turn_off(gpio)
                     sys.exit(1)
                 else:
-                    with GPIO(pins) as gpio:
+                    with GPIO(light_pins) as gpio:
+                        turn_off(gpio)
+                    with GPIO(motor_pins) as gpio:
+                        turn_on(gpio)
+                        time.sleep(3)
                         turn_off(gpio)
                     # filename = ~/sys/class/gpio/gpio12
                     # os.system('./Electronics/turnOnGPIO.sh')
 
+
 if __name__ == "__main__":
     main()
-
