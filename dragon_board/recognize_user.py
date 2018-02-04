28649ea1e2e67531316b8f52227b3f0fdb42082f
Patrick Liu <pcjl@users.noreply.github.com>
2018-02-04 05:31:52 -0500

    Clean up code for dragon board

--- dragon_board/recognize_user.py
+++ dragon_board/recognize_user.py
@@ -3,47 +3,47 @@ import json
 import sys
 import requests
 import os
-import time
 
 DEVICE_NUMBER = 0
 IMAGE_FILE = "output.jpg"
+API = "http://ec2-18-219-99-191.us-east-2.compute.amazonaws.com:5000/verify"
 no_face = True
 
-while no_face:	
-	vc = cv2.VideoCapture(DEVICE_NUMBER)
-	retVal, frame = vc.read()
-	small = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
-	cv2.imwrite(IMAGE_FILE, small)	
-	vc.release()
+while no_face:
+    vc = cv2.VideoCapture(DEVICE_NUMBER)
+    retVal, frame = vc.read()
+    small = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    cv2.imwrite(IMAGE_FILE, small)
+    vc.release()
 
-	print("done reading image")
+    print("Done reading image.")
 
-	response = requests.post("http://ec2-18-219-99-191.us-east-2.compute.amazonaws.com:5000/verify",files={"image":open('/home/linaro/Documents/AWS_IOT_Test/output.jpg', 'r')})
-	
-	print("done web response")
+    response = requests.post(API, files={
+        "image": open(
+            '/home/linaro/Documents/AWS_IOT_Test/haven/output.jpg', 'r')})
 
-	if response.status_code == 200:
-		user = json.loads(response.text)['user']
-		confidence = json.loads(response.text)['confidence']	
-		
-		if confidence > 0.95:
-			#homeActions
-			first = user.split("-")[0]
-			last = user.split("-")[1]			
-			print(first + " " + last + " is home")
-			
-			#filename = sys/class/gpio/gpio36	
-			#os.system('./Electronics/turnOnGPIO.sh')
-			#filename = sys/class/gpio/gpio12
-			#os.system('./Electronics/turnOnGPIO.sh')
+    print("Received web response.")
 
-			k = raw_input("")
-			if k == 'q':
-				sys.exit(1)
-			#else:
-				#filename = ~/sys/class/gpio/gpio36	
-				#os.system('./Electronics/turnOnGPIO.sh')
-				#filename = ~/sys/class/gpio/gpio12
-				#os.system('./Electronics/turnOnGPIO.sh')		
+    if response.status_code == 200:
+        user = json.loads(response.text)['user']
+        confidence = json.loads(response.text)['confidence']
 
+        if confidence > 0.95:
+            # home_actions(user)
+            first = user.split("-")[0]
+            last = user.split("-")[1]
+            print(first + " " + last + " recognised.")
 
+            # filename = sys/class/gpio/gpio36
+            # os.system('./Electronics/turnOnGPIO.sh')
+            # filename = sys/class/gpio/gpio12
+            # os.system('./Electronics/turnOnGPIO.sh')
+
+            k = raw_input("")
+            if k == 'q':
+                sys.exit(1)
+            # else:
+                # filename = ~/sys/class/gpio/gpio36
+                # os.system('./Electronics/turnOnGPIO.sh')
+                # filename = ~/sys/class/gpio/gpio12
+                # os.system('./Electronics/turnOnGPIO.sh')
